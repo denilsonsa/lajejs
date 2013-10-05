4f2f1e7000542c54680dbf4fb0e30eb57dd78589
Denilson Sá <denilsonsa@gmail.com>
2013-10-04 23:40:52 -0300

    Nested SVG.
    Yo Dawg, I herd you like SVG, so I put SVG in your SVG so you can SVG while you SVG in your HTML.

--- laje.js
+++ laje.js
@@ -2,36 +2,20 @@
 
 "use strict";
 
-
-var fig_laje_element;
-
-// Grabs the model <div> from the document, stores in a JS var, and removes it
-// from the document.
-function initFigLaje() {
-	fig_laje_element = document.getElementById('fig-laje-model');
-	if (!fig_laje_element) {
-		console.error('Cannot find #fig-laje-model');
-		return;
-	}
-	fig_laje_element.removeAttribute('id');
-	var parent = fig_laje_element.parentNode;
-	if (parent) {
-		parent.removeChild(fig_laje_element);
-	}
-}
-
 // Returns a new copy of <div class="fig-laje">.
 // Optionally adds a class "fig-laje-<number>", if the number is positive.
 function newFigLaje(number) {
-	if (!fig_laje_element) {
-		console.error('Fatal error: fig_laje_element is ', fig_laje_element);
+	var model = document.getElementById('fig-laje-model');
+	if (!model) {
+		console.error('Cannot find #fig-laje-model');
 		return;
 	}
-	var newFig = fig_laje_element.cloneNode(true);  // deep = true
+	var new_fig = model.cloneNode(true);  // deep = true
+	new_fig.removeAttribute('id');
 	if (number && number > 0) {
-		newFig.classList.add('fig-laje-' + number);
+		new_fig.classList.add('fig-laje-' + number);
 	}
-	return newFig;
+	return new_fig;
 }
 
 // Given a <div class="fig-laje"> and optionally a positive number, reset the
@@ -51,8 +35,6 @@ function setFigLajeNumber(elem, number) {
 
 
 function onLoadHandler() {
-	initFigLaje();
-
 	var foo = document.querySelectorAll('fieldset.tipo-laje label');
 	for (var i = 0; i < foo.length; i++) {
 		var label = foo[i];
@@ -60,9 +42,6 @@ function onLoadHandler() {
 		var fig = newFigLaje(number);
 		label.appendChild(fig);
 	}
-	var fig = newFigLaje();
-	var diagrama = document.querySelector('form .diagrama');
-	diagrama.appendChild(fig);
 }
 
 window.addEventListener('load', onLoadHandler, false);
